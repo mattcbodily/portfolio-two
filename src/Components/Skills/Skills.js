835ad87289f6f83382ef78e9004728344baaa4a7
Matthew Bodily <mattcbodily@gmail.com>
2019-08-01 15:30:38 -0600

    added the rest of the file structure

---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Skills.css';
+
+const Skills = () => {
+    return(
+        <div className='skills-container'>
+            Skills Component
+        </div>
+    )
+}
+
+export default Skills;
