503424b4020f71b93ef03a079bf732ad333cc72c
Matthew Bodily <mattcbodily@gmail.com>
2019-08-02 23:43:13 -0600

    added profile picture and added content to the about component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import PortfolioPicture from './../../assets/Portfoliopicture.jpg';
 import './About.css';
 
 const About = () => {
     return(
         <div id='about' className='about-container'>
-            About Component
+            <img 
+                src={PortfolioPicture} 
+                alt='Matt Bodily' 
+                className='portfolio-picture' />
+            <p>Detail oriented, hard working, and driven to succeed, 
+               I love the problem solving and daily learning that is involved in the web devlopment profession, and love an 
+               environment where I can work with a team. Outside of code, I love watching sports, playing the drums, visiting 
+               the National Parks, and spending time with my family.
+            </p>
         </div>
     )
 }
