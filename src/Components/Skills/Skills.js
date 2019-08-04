75a0e83f32af94db62e1dcef0069088d3beeb6be
Matthew Bodily <mattcbodily@gmail.com>
2019-08-03 23:35:37 -0600

    added text to the about component, as well as added some content to the skills component

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Skills.css';
+import JavascriptIcon from '../../assets/javascript3.png';
 
 const Skills = () => {
     return(
         <div id='skills' className='skills-container'>
-            Skills Component
+            <h1>Development Skills</h1>
+            <h3>Front-end</h3>
+            <p><img 
+                src={JavascriptIcon} 
+                alt='Javascript Icon'
+                className='skill-icon'/> Javascript</p>
+            <h3>Back-end</h3>
+            <h3>Other</h3>
         </div>
     )
 }
