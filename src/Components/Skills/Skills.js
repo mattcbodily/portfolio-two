8b081c838bc31366022f1db15119b88554e730eb
Matthew Bodily <mattcbodily@gmail.com>
2019-08-03 23:48:22 -0600

    added more skill icons to the skills component

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Skills.css';
 import JavascriptIcon from '../../assets/javascript3.png';
+import HtmlIcon from '../../assets/html5.png';
+import CssIcon from '../../assets/css3.png';
+import ReactIcon from '../../assets/react2.png';
 
 const Skills = () => {
     return(
@@ -11,6 +14,18 @@ const Skills = () => {
                 src={JavascriptIcon} 
                 alt='Javascript Icon'
                 className='skill-icon'/> Javascript</p>
+            <p><img 
+                src={HtmlIcon} 
+                alt='HTML Icon'
+                className='skill-icon'/> HTML</p>
+            <p><img 
+                src={CssIcon} 
+                alt='CSS Icon'
+                className='skill-icon'/> CSS</p>
+            <p><img 
+                src={ReactIcon} 
+                alt='React Icon'
+                className='skill-icon'/> React</p>
             <h3>Back-end</h3>
             <h3>Other</h3>
         </div>
