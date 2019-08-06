6ff2499890d5774c4607f790d72bc4a5367c5ca3
Matthew Bodily <mattcbodily@gmail.com>
2019-08-06 14:59:19 -0600

    added the PearProgramming video to the projects page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Projects.css';
 import TeachioVideo from '../../assets/videos/Teachio.mp4';
+import PearProgrammingVideo from '../../assets/videos/PearProgramming.mp4';
 
 const Projects = () => {
     return(
@@ -9,6 +10,14 @@ const Projects = () => {
             <h3>Teachio</h3>
             <video className='project-video' controls>
                 <source src={TeachioVideo} type='video/mp4'/>
+            </video>
+            <h3>PearProgramming</h3>
+            <video className='project-video' controls>
+                <source src={PearProgrammingVideo} type='video/mp4'/>
+            </video>
+            <h3>O.D.N.T. Longboards</h3>
+            <video className='project-video' controls>
+                <source src={PearProgrammingVideo} type='video/mp4'/>
             </video>
         </div>
     )
