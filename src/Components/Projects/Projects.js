042758954c46a218f45778434bbfffe6c977b09e
Matthew Bodily <mattcbodily@gmail.com>
2019-08-06 14:53:05 -0600

    added the Teachio video to the projects component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Projects.css';
+import TeachioVideo from '../../assets/videos/Teachio.mp4';
 
 const Projects = () => {
     return(
         <div id='projects' className='projects-container'>
-            Projects Component
+            <h1>Projects</h1>
+            <h3>Teachio</h3>
+            <video className='project-video' controls>
+                <source src={TeachioVideo} type='video/mp4'/>
+            </video>
         </div>
     )
 }
