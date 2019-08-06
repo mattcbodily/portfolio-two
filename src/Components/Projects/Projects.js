0bbc6035d5b50dc3e5fbd3a7f5bf464962d6acbe
Matthew Bodily <mattcbodily@gmail.com>
2019-08-06 15:26:47 -0600

    added the odnt video and piggybank video to the Projects component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Projects.css';
 import TeachioVideo from '../../assets/videos/Teachio.mp4';
 import PearProgrammingVideo from '../../assets/videos/PearProgramming.mp4';
+import ODNTLongboardsVideo from '../../assets/videos/odntshop.mp4';
+import PiggyBankVideo from '../../assets/videos/piggy_bank_two.mp4';
 
 const Projects = () => {
     return(
@@ -17,7 +19,11 @@ const Projects = () => {
             </video>
             <h3>O.D.N.T. Longboards</h3>
             <video className='project-video' controls>
-                <source src={PearProgrammingVideo} type='video/mp4'/>
+                <source src={ODNTLongboardsVideo} type='video/mp4'/>
+            </video>
+            <h3>PiggyBank</h3>
+            <video className='project-video' controls>
+                <source src={PiggyBankVideo} type='video/mp4'/>
             </video>
         </div>
     )
