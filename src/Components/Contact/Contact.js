4cfd6135fe9fb4cb41bf340914a0da222d8a508a
Matthew Bodily <mattcbodily@gmail.com>
2019-08-08 09:25:21 -0600

    added a new favicon and added the keywords, description, and author meta tags to index.html in the public folder

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Contact.css';
 
 const Contact = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
     return(
         <div id='contact' className='contact-container'>
-            Contact Component
+            <h1>Contact</h1>
+            <h4>Have a question or want to work together?</h4>
+            <form>
+                <input 
+                    value={name}
+                    placeholder='Name'
+                    className='contact-input'
+                    onChange={(e) => setName(e.target.value)}/>
+                <input 
+                    value={email}
+                    placeholder='Email'
+                    className='contact-input'
+                    onChange={(e) => setEmail(e.target.value)}/>
+                <textarea 
+                    value={message}
+                    placeholder='Enter Message'
+                    className='contact-textarea'
+                    onChange={(e) => setMessage(e.target.value)}/>
+            </form>
         </div>
     )
 }
