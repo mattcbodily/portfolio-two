80cbd2e671bd5d1ed017d230cc5cfc7090b2f6cd
Matthew Bodily <mattcbodily@gmail.com>
2019-08-19 16:48:30 -0600

    Adjusted styling to the About component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,23 +9,25 @@ const About = () => {
                 src={PortfolioPicture} 
                 alt='Matt Bodily' 
                 className='portfolio-picture' />
-            <h3>About Me</h3>
-            <p>Detail oriented, hard working, and driven to succeed, 
-               I love the problem solving and daily learning that is involved in the web devlopment profession, and love an 
-               environment where I can work with a team. Outside of code, I love watching sports, playing the drums, visiting 
-               the National Parks, and spending time with my family.
-            </p>
+            <div>
+                <h3>About Me</h3>
+                <p>Detail oriented, hard working, and driven to succeed, 
+                I love the problem solving and daily learning that is involved in the web devlopment profession, and love an 
+                environment where I can work with a team. Outside of code, I love watching sports, playing the drums, visiting 
+                the National Parks, and spending time with my family.
+                </p>
                 <h3>Education</h3>
-            <section className='education-block'>
-                <p>DevMountain</p>
-                <p className='education-italics'>Web Badged</p>
-                <p className='education-italics'>Jan. 2019 - April 2019</p>
-            </section>
-            <section className='education-block'>
-                <p>Utah Valley University</p>
-                <p className='education-italics'>Associates in Integrated Studies</p>
-                <p className='education-italics'>Jan. 2013 - Dec. 2016</p>
-            </section>
+                <section className='education-block'>
+                    <p className='institution'>DevMountain</p>
+                    <p className='education-italics'>Web Badged</p>
+                    <p className='education-italics'>Jan. 2019 - April 2019</p>
+                </section>
+                <section className='education-block'>
+                    <p className='institution'>Utah Valley University</p>
+                    <p className='education-italics'>Associates in Integrated Studies</p>
+                    <p className='education-italics'>Jan. 2013 - Dec. 2016</p>
+                </section>
+            </div>
         </div>
     )
 }
