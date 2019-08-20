e67e9ac33ce33611fcfaa2acd638ea56d959ddc0
Matthew Bodily <mattcbodily@gmail.com>
2019-08-20 15:21:20 -0600

    added some more changes to the About component so that the text containers only have a max-width, and the about container is displayed inline-block

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,14 +10,16 @@ const About = () => {
                 alt='Matt Bodily' 
                 className='portfolio-picture' />
             <div>
-                <h3>About Me</h3>
-                <p>Detail oriented, hard working, and driven to succeed, 
-                I love the problem solving and daily learning that is involved in the web devlopment profession, and love an 
-                environment where I can work with a team. Outside of code, I love watching sports, playing the drums, visiting 
-                the National Parks, and spending time with my family.
-                </p>
-                <h3>Education</h3>
+                <section className='about-me-section'>
+                    <h3>About Me</h3>
+                    <p className='about-me-p'>Detail oriented, hard working, and driven to succeed, 
+                    I love the problem solving and daily learning that is involved in the web devlopment profession, and love an 
+                    environment where I can work with a team. Outside of code, I love watching sports, playing the drums, visiting 
+                    the National Parks, and spending time with my family.
+                    </p>
+                </section>
                 <section className='education-block'>
+                    <h3>Education</h3>
                     <p className='institution'>DevMountain</p>
                     <p className='education-italics'>Web Badged</p>
                     <p className='education-italics'>Jan. 2019 - April 2019</p>
