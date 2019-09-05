566bb1fff5e30a17889c2b9771a1922ee3085ce0
Matthew Bodily <mattcbodily@gmail.com>
2019-09-05 11:06:31 -0600

    connected db to the server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
-const express = require('express');
-const massive = require('massive');
-const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const express = require('express'),
+      massive = require('massive'),
+      {SERVER_PORT, CONNECTION_STRING} = process.env,
+      app = express();
 
-const app = express();
 app.use(express.json());
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    console.log('DB Connected')
+}).catch(err => console.log(err))
 
 app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`));
